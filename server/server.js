158e24b90997904dd72430223d52065f8f56fb99
Vishal P <vishalprakaash@gmail.com>
2021-03-10 00:03:14 +0530

    image upload to cloudinary and link of the url saved in  database

--- server/server.js
+++ server/server.js
@@ -3,17 +3,15 @@ const { ApolloServer } = require('apollo-server-express');
 const http = require('http');
 const path = require('path');
 const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 const { fileLoader, mergeTypes, mergeResolvers } = require('merge-graphql-schemas');
 require('dotenv').config();
-const { authCheck } = require('./helpers/auth');
+const { authCheckMiddleware } = require('./helpers/auth');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cloudinary = require('cloudinary');
 
 // express server
 const app = express();
-// middlewares
-app.use(cors());
-app.use(bodyParser.json({ limit: '10mb' }));
 
 // db
 const db = async () => {
@@ -31,6 +29,10 @@ const db = async () => {
 };
 // execute database connection
 db();
+
+// middlewares
+app.use(cors());
+app.use(bodyParser.json({ limit: '5mb' }));
 
 // typeDefs
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './typeDefs')));
@@ -51,9 +53,45 @@ apolloServer.applyMiddleware({ app });
 const httpserver = http.createServer(app);
 
 // rest endpoint
-app.get('/rest', authCheck, function(req, res) {
+app.get('/rest', authCheckMiddleware, function(req, res) {
     res.json({
         data: 'you hit rest endpoint great!'
+    });
+});
+
+// cloudinary config
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+// upload
+app.post('/uploadimages', authCheckMiddleware, (req, res) => {
+    cloudinary.uploader.upload(
+        req.body.image,
+        (result) => {
+            console.log(result);
+            res.send({
+                // url: result.url,
+                url: result.secure_url,
+                public_id: result.public_id
+            });
+        },
+        {
+            public_id: `${Date.now()}`, // public name
+            resource_type: 'auto' // JPEG, PNG
+        }
+    );
+});
+
+// remove image
+app.post('/removeimage', authCheckMiddleware, (req, res) => {
+    let image_id = req.body.public_id;
+
+    cloudinary.uploader.destroy(image_id, (error, result) => {
+        if (error) return res.json({ success: false, error });
+        res.send('ok');
     });
 });
 
